ec717a8bfb610037dc6122ae8541a358d634dfb0
johnlocke956 <francescograsso.business@gmail.com>
2022-09-28 21:04:51 +0200

    Implemented routing and navbar

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -13,7 +13,7 @@ const CategoryItem = ({ category }) => {
       />
       <div className='category-body-container'>
         <h2>{title}</h2>
-        <p>{subtitle}</p>
+        <p>Shop now</p>
       </div>
     </div>
   );
